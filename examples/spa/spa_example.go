8fd28ce7dcc4aee3aa06cd6eacfe034126dd35e6
soypat <graded.sp@gmail.com>
2022-01-18 09:11:32 -0300

    SPA fleshed out. still some bugs

--- examples/spa/spa_example.go
+++ examples/spa/spa_example.go
@@ -4,6 +4,8 @@ import (
 	"github.com/hexops/vecty"
 	"github.com/hexops/vecty/elem"
 	"github.com/soypat/mdc"
+	"github.com/soypat/mdc/examples/jlog"
+	"github.com/soypat/mdc/icons"
 )
 
 const (
@@ -14,6 +16,7 @@ const (
 var globalListener func()
 
 func main() {
+	jlog.PackageLevel = jlog.LevelTrace
 	mdc.SetDefaultViewport()
 	mdc.AddDefaultStyles()
 	mdc.AddDefaultScripts()
@@ -30,12 +33,31 @@ type Body struct {
 }
 
 func (b *Body) Render() vecty.ComponentOrHTML {
-	navbar := &mdc.Navbar{
-		SectionStart: vecty.List{
-			&mdc.Typography{Root: vecty.Text(title), Style: mdc.Headline6},
+	spa := &mdc.SPA{
+		FullHeightDrawer: false,
+		Navbar: &mdc.Navbar{
+			// Variant: mdc.VariantTopBarFixed,
+			SectionStart: vecty.List{
+				&mdc.Typography{Root: vecty.Text(title), Style: mdc.Headline6},
+			},
 		},
+		Drawer: &mdc.Leftbar{
+			// Title:       vecty.Text(title),
+			// Subtitle:    vecty.Text(motto),
+			Dismissible: true,
+			List: &mdc.List{
+				ListElem: mdc.ElementDivList,
+				List: vecty.List{
+					&mdc.ListItem{Label: vecty.Text("Visit our hot sale"), Icon: icons.PointOfSale},
+					&mdc.ListItem{Label: vecty.Text("Our mission"), Icon: icons.AirplanemodeActive},
+					&mdc.ListItem{Label: vecty.Text("Our values"), Icon: icons.PersonalInjury},
+					&mdc.ListItem{Label: vecty.Text("Contact us"), Icon: icons.ContactPhone},
+				},
+			},
+		},
+		Content: &mdc.Typography{Root: vecty.Text("The future, all in one place. Now. Buy now.")},
 	}
 	return elem.Body(
-		navbar,
+		spa,
 	)
 }
